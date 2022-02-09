1ca975b5f82f1c3691777ba211cc8b04bb5d2bf7
Arjohn Ely <arjohn.ely@fligno.com>
2022-02-09 16:08:43 +0800

    update

--- app/Models/otherInformation.php
+++ app/Models/otherInformation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class otherInformation extends Model
+class OtherInformation extends Model
 {
     use HasFactory;
 }
